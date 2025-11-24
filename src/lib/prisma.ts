d4352410497042477b6f55457f157b622185740f
Sergio <sergio.costa_ads@outlook.com>
2025-11-24 12:48:33 +0000

    fix: lib/prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,18 @@
-import { PrismaClient } from "../../generated/prisma";
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+    prisma?: PrismaClient;
+};
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    let globalWithPrisma = global as typeof globalThis & {
-        prisma: PrismaClient;
-    }
-    if (!globalWithPrisma.prisma) {
-        globalWithPrisma.prisma = new PrismaClient();
-    }
-    prisma = globalWithPrisma.prisma;
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["query", "info", "warn", "error"],
+    });
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
