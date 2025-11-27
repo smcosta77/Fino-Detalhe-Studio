913e9f56ce08bb3da97ce35af1833229a5393df2
Sergio <sergio.costa_ads@outlook.com>
2025-11-27 22:58:23 +0000

    alterado variaveis

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-// src/lib/auth.ts
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -23,8 +22,9 @@ export const authConfig = {
 
     providers: [
         Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+            clientId: process.env.AUTH_GOOGLE_ID!,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+            checks: ["state"],
         }),
     ],
 
